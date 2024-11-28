dc84329b794bb065e885d69d322479fcf18b81c5
Ochiponu-Awodi <awodiomale@gmail.com>
2024-11-28 15:40:39 +0100

    Pass Data to Views

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        title: 'Welcome',
+        message: 'Hello from EJS'
+    });
 })
 
 app.listen(8000, () => console.log('Server started'));
